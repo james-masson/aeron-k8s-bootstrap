ca21d2956db1615982e76f1b0324eaf3f4096218
James Masson <james.masson@jmips.co.uk>
2025-11-15 15:37:35 +0000

    Fix Protobuf advisory & improve caching (#10)

--- build_containers.sh
+++ build_containers.sh
@@ -2,5 +2,5 @@
 
 set -eo pipefail
 
-docker build -t jmips/aeron-k8s-bootstrap .
-docker build -t jmips/aeronmd -f Dockerfile-aeronmd .
+docker build -t ghcr.io/james-masson/aeron-k8s-bootstrap/aeron-k8s-bootstrap .
+docker build -t ghcr.io/james-masson/aeron-k8s-bootstrap/aeronmd -f Dockerfile-aeronmd .
--- check_bootstrap_success.sh
+++ check_bootstrap_success.sh
@@ -2,16 +2,40 @@
 
 set -eo pipefail
 
-OUTPUT="$(kubectl exec -it example-aeron-k8s-bootstrap-2 -c media-driver --   AeronStat -w false | grep "Resolver neighbors" | awk '{print $3}')"
+function f_check (){
 
-# We expect two neighbours
-if [[ ${OUTPUT} != "2" ]]
-then
-    echo "*** Bootstrap failure ***"
-    kubectl logs example-aeron-k8s-bootstrap-2 -c media-driver
-    kubectl logs example-aeron-k8s-bootstrap-2 -c aeron-k8s-bootstrap
-    kubectl exec -it example-aeron-k8s-bootstrap-2 -c media-driver --   AeronStat -w false
-    exit 1
-fi
+    OUTPUT="$(kubectl exec -it example-aeron-k8s-bootstrap-2 -c media-driver --   AeronStat -w false | grep "Resolver neighbors" | awk '{print $3}')"
 
-echo "** Bootstrap successful **"
+    # We expect two neighbours
+    if [[ ${OUTPUT} != "2" ]]
+    then
+        return 1
+    fi
+
+    return 0
+}
+
+# Retry logic: try up to 5 times with 2 second backoff
+MAX_ATTEMPTS=5
+BACKOFF_SECONDS=2
+
+for attempt in $(seq 1 $MAX_ATTEMPTS); do
+    echo "Attempt $attempt of $MAX_ATTEMPTS..."
+
+    if f_check; then
+        echo "** Bootstrap successful **"
+        exit 0
+    fi
+
+    if [[ $attempt -lt $MAX_ATTEMPTS ]]; then
+        echo "Check failed, retrying in ${BACKOFF_SECONDS} seconds..."
+        sleep $BACKOFF_SECONDS
+    fi
+done
+
+# All attempts failed
+echo "*** Bootstrap failure after $MAX_ATTEMPTS attempts ***"
+kubectl logs example-aeron-k8s-bootstrap-2 -c media-driver
+kubectl logs example-aeron-k8s-bootstrap-2 -c aeron-k8s-bootstrap
+kubectl exec -it example-aeron-k8s-bootstrap-2 -c media-driver --   AeronStat -w false
+exit 1
