586fefee2e123a95c102f7fca4ad3067c33e5494
James Masson <james.masson@jmips.co.uk>
2025-11-15 14:50:11 +0000

    Fix caching and wait a bit longer for bootstrap

--- build_containers.sh
+++ build_containers.sh
@@ -4,3 +4,11 @@ set -eo pipefail
 
 docker build -t jmips/aeron-k8s-bootstrap .
 docker build -t jmips/aeronmd -f Dockerfile-aeronmd .
+
+# If running in github actions, also save the containers to disk for later caching
+if test -z "${GITHUB_JOB}"
+then
+    mkdir /tmp/container-cache
+    docker save jmips/aeron-k8s-bootstrap -o /tmp/container-cache/aeron-k8s-bootstrap
+    docker save jmips/aeronmd -o /tmp/container-cache/aeronmd
+fi
